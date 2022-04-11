00180819a9e0eaa734a4d07ea9b8e630b3c1b6e8
Tatiana <lastra.tatiana@gmail.com>
2022-04-10 20:52:02 -0400

    detalles terminados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 $(function () {
-    $('[data-bs-toggle="tooltip"]').tooltip();
-  });
-  
-  $(document).scroll(function () {
-    const y = $("html").scrollTop();
-  
-    y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
-  });
+  $('[data-bs-toggle="tooltip"]').tooltip();
+});
+
+$(document).scroll(function () {
+  const y = $("html").scrollTop();
+  y > 250 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
+});
